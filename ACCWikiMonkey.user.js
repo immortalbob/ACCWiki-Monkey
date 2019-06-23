ae5921fce84ed2ab66af8fef872763568c5e1573
ORDER\Mike <immortalbob@gmail.com>
2019-06-23 10:22:34 -0400

    initial commit
    simple script for great results

---
+++ ACCWikiMonkey.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        ACCWikiMonkey
+// @namespace   Immortalbob
+// @include     *asheron.fandom.com*
+// @version     1
+// @grant       none
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// ==/UserScript==
+$('.mcf-content').remove();
+$('.WikiaRail').remove();
+$('.home-top-right-ads').remove();
+$('.WikiaTopAds').remove();
+
